55639ebe9f8e9fed3fc040976c01986d8a91252b
Kogermanov <157327091+Kogermanov@users.noreply.github.com>
2025-12-03 13:13:45 +0700

    Update Program.cs

--- lab1/Program.cs
+++ lab1/Program.cs
@@ -1,1 +1,26 @@
+using System.Net;
+using System;
 
+namespace UdpMulticastProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Start program");
+
+            string? multicastAddress = Console.ReadLine();
+
+            IPAddress? multicastIP;
+            if (!IPAddress.TryParse(multicastAddress, out multicastIP))
+            {
+                Console.WriteLine("bad format multicast address.");
+                return;
+            }
+
+            UdpMulticast socket = new UdpMulticast(multicastIP);
+            socket.SearchMulticastCopies();
+        }
+    }
+
+}
